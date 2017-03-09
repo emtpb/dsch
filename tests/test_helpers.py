16255191a5afde3523eb57159b0ae391ec3e1fc1
Manuel Webersen <webersen@emt.uni-paderborn.de>
2017-03-09 14:22:30 +0100

    Simplify getting backend module from name.
    dsch.helpers.backend_module() now yields the backend module for a given
    backend name.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,12 @@
 from dsch import helpers
 
 
+def test_backend_module():
+    import dsch.backends.npz
+    backend_module = helpers.backend_module('npz')
+    assert backend_module == dsch.backends.npz
+
+
 def test_inflate_dotted():
     input = {
         'foo.foo.foo': 23,
