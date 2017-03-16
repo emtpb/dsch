517f80a3c9537e8daf4ebbaf67e6ac5440e031b4
Manuel Webersen <webersen@emt.uni-paderborn.de>
2017-03-16 09:11:43 +0100

    Use Array in the high-level tests.

--- tests/test_high_level.py
+++ tests/test_high_level.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from dsch import frontend, schema
 
@@ -9,6 +10,19 @@ def storage_path(request, tmpdir):
     elif request.param == 'npz':
         storage_path = str(tmpdir.join('test_file.npz'))
     return storage_path
+
+
+def test_array(storage_path, tmpdir):
+    schema_node = schema.Array(dtype='int')
+    storage = frontend.create(storage_path=storage_path,
+                              schema_node=schema_node)
+    storage.data.replace(np.array([23, 42]))
+    storage.data.validate()
+    storage.save()
+
+    new_storage = frontend.load(storage_path)
+    data = new_storage.data
+    assert np.all(data.value == np.array([23, 42]))
 
 
 def test_bool(storage_path, tmpdir):
@@ -26,15 +40,19 @@ def test_bool(storage_path, tmpdir):
 
 def test_compilation(storage_path, tmpdir):
     schema_node = schema.Compilation({
+        'test_array': schema.Array(dtype='int'),
         'test_bool': schema.Bool(),
         'test_string': schema.String(),
         'test_comp': schema.Compilation({
+            'comp_array': schema.Array(dtype='int'),
             'comp_bool': schema.Bool(),
             'comp_string': schema.String()
         }),
+        'test_list_array': schema.List(schema.Array(dtype='int')),
         'test_list_bool': schema.List(schema.Bool()),
         'test_list_string': schema.List(schema.String()),
         'test_complist': schema.List(schema.Compilation({
+            'complist_array': schema.Array(dtype='int'),
             'complist_bool': schema.Bool(),
             'complist_string': schema.String()
         })),
@@ -44,15 +62,27 @@ def test_compilation(storage_path, tmpdir):
     storage = frontend.create(storage_path=storage_path,
                               schema_node=schema_node)
 
+    storage.data.test_array.replace(np.array([23, 42]))
     storage.data.test_bool.replace(True)
     storage.data.test_string.replace('spam')
+    storage.data.test_comp.comp_array.replace(np.array([23, 42]))
     storage.data.test_comp.comp_bool.replace(True)
     storage.data.test_comp.comp_string.replace('eggs')
+    storage.data.test_list_array.replace([np.array([23, 42]),
+                                          np.array([1, 0])])
     storage.data.test_list_bool.replace([True, False])
     storage.data.test_list_string.replace(['spam', 'eggs'])
     storage.data.test_complist.replace([
-        {'complist_bool': True, 'complist_string': 'spam'},
-        {'complist_bool': False, 'complist_string': 'eggs'},
+        {
+            'complist_array': np.array([23, 42]),
+            'complist_bool': True,
+            'complist_string': 'spam'
+        },
+        {
+            'complist_array': np.array([1, 0]),
+            'complist_bool': False,
+            'complist_string': 'eggs'
+        },
     ])
     storage.data.test_listlist.replace([[True, False], [False, True]])
     storage.data.validate()
@@ -60,16 +90,24 @@ def test_compilation(storage_path, tmpdir):
 
     new_storage = frontend.load(storage_path)
     data = new_storage.data
+    assert np.all(data.test_array.value == np.array([23, 42]))
     assert data.test_bool.value is True
     assert data.test_string.value == 'spam'
+    assert np.all(data.test_comp.comp_array.value == np.array([23, 42]))
     assert data.test_comp.comp_bool.value is True
     assert data.test_comp.comp_string.value == 'eggs'
+    assert np.all(data.test_list_array[0].value == np.array([23, 42]))
+    assert np.all(data.test_list_array[1].value == np.array([1, 0]))
     assert data.test_list_bool[0].value is True
     assert data.test_list_bool[1].value is False
     assert data.test_list_string[0].value == 'spam'
     assert data.test_list_string[1].value == 'eggs'
+    assert np.all(data.test_complist[0].complist_array.value == np.array([23,
+                                                                          42]))
     assert data.test_complist[0].complist_bool.value is True
     assert data.test_complist[0].complist_string.value == 'spam'
+    assert np.all(data.test_complist[1].complist_array.value == np.array([1,
+                                                                          0]))
     assert data.test_complist[1].complist_bool.value is False
     assert data.test_complist[1].complist_string.value == 'eggs'
     assert data.test_listlist[0][0].value is True
@@ -81,12 +119,15 @@ def test_compilation(storage_path, tmpdir):
 def test_list(storage_path, tmpdir):
     schema_node = schema.List(
         schema.Compilation({
+            'test_array': schema.Array(dtype='int'),
             'test_bool': schema.Bool(),
             'test_string': schema.String(),
             'test_comp': schema.Compilation({
+                'comp_array': schema.Array(dtype='int'),
                 'comp_bool': schema.Bool(),
                 'comp_string': schema.String()
             }),
+            'test_list_array': schema.List(schema.Array(dtype='int')),
             'test_list_bool': schema.List(schema.Bool()),
             'test_list_string': schema.List(schema.String()),
             'test_listlist': schema.List(schema.List(schema.Bool())),
@@ -96,18 +137,26 @@ def test_list(storage_path, tmpdir):
                               schema_node=schema_node)
 
     storage.data.append()
+    storage.data[0].test_array.replace(np.array([23, 42]))
     storage.data[0].test_bool.replace(True)
     storage.data[0].test_string.replace('spam')
+    storage.data[0].test_comp.comp_array.replace(np.array([23, 42]))
     storage.data[0].test_comp.comp_bool.replace(True)
     storage.data[0].test_comp.comp_string.replace('eggs')
+    storage.data[0].test_list_array.replace([np.array([23, 42]),
+                                             np.array([1, 0])])
     storage.data[0].test_list_bool.replace([True, False])
     storage.data[0].test_list_string.replace(['spam', 'eggs'])
     storage.data[0].test_listlist.replace([[True, False], [False, True]])
     storage.data.append()
+    storage.data[1].test_array.replace(np.array([23, 42]))
     storage.data[1].test_bool.replace(True)
     storage.data[1].test_string.replace('spam')
+    storage.data[1].test_comp.comp_array.replace(np.array([23, 42]))
     storage.data[1].test_comp.comp_bool.replace(True)
     storage.data[1].test_comp.comp_string.replace('eggs')
+    storage.data[1].test_list_array.replace([np.array([23, 42]),
+                                             np.array([1, 0])])
     storage.data[1].test_list_bool.replace([True, False])
     storage.data[1].test_list_string.replace(['spam', 'eggs'])
     storage.data[1].test_listlist.replace([[True, False], [False, True]])
@@ -116,10 +165,14 @@ def test_list(storage_path, tmpdir):
 
     new_storage = frontend.load(storage_path)
     for data in new_storage.data:
+        assert np.all(data.test_array.value == np.array([23, 42]))
         assert data.test_bool.value is True
         assert data.test_string.value == 'spam'
+        assert np.all(data.test_comp.comp_array.value == np.array([23, 42]))
         assert data.test_comp.comp_bool.value is True
         assert data.test_comp.comp_string.value == 'eggs'
+        assert np.all(data.test_list_array[0].value == np.array([23, 42]))
+        assert np.all(data.test_list_array[1].value == np.array([1, 0]))
         assert data.test_list_bool[0].value is True
         assert data.test_list_bool[1].value is False
         assert data.test_list_string[0].value == 'spam'
