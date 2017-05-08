98a7d6f5e52c0bd70f3d38373bb20b5f8292e5a0
Manuel Webersen <webersen@emt.uni-paderborn.de>
2017-05-08 14:55:16 +0200

    Extend frontend tests to all backends.

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -1,52 +1,63 @@
+from collections import namedtuple
+import importlib
 import pytest
 from dsch import frontend, schema
-from dsch.backends import npz
 
 
-def test_create(tmpdir):
+backend_data = namedtuple('backend_data', ('module', 'storage_path'))
+
+
+@pytest.fixture(params=('hdf5', 'mat', 'npz'))
+def backend(request, tmpdir):
+    backend = backend_data(
+        module=importlib.import_module('dsch.backends.' + request.param),
+        storage_path=str(tmpdir.join('test_frontend.' + request.param))
+    )
+    return backend
+
+
+def test_create(backend):
     schema_node = schema.Bool()
-    storage_path = str(tmpdir.join('test_create.npz'))
-    storage = frontend.create(storage_path, schema_node)
-    assert isinstance(storage, npz.Storage)
+    storage = frontend.create(backend.storage_path, schema_node)
+    assert isinstance(storage, backend.module.Storage)
     assert storage.schema_node == schema_node
 
 
-def test_load(tmpdir):
+def test_load(backend):
     schema_node = schema.Bool()
-    storage_path = str(tmpdir.join('test_load.npz'))
-    storage = frontend.create(storage_path, schema_node)
+    storage = frontend.create(backend.storage_path, schema_node)
     storage.data.replace(True)
     storage.save()
 
-    new_storage = frontend.load(storage_path)
-    assert isinstance(new_storage, npz.Storage)
+    new_storage = frontend.load(backend.storage_path)
+    assert isinstance(new_storage, backend.module.Storage)
     assert new_storage.data.value is True
 
 
-def test_load_require_schema(tmpdir):
+def test_load_require_schema(backend):
     schema_node = schema.Bool()
-    storage_path = str(tmpdir.join('test_load.npz'))
-    storage = frontend.create(storage_path, schema_node)
+    storage = frontend.create(backend.storage_path, schema_node)
     storage.data.replace(True)
     storage.save()
+
     schema_hash = storage.schema_hash()
-    frontend.load(storage_path, require_schema=schema_hash)
+    frontend.load(backend.storage_path, require_schema=schema_hash)
 
 
-def test_load_require_valid(tmpdir):
+def test_load_require_valid(backend):
     schema_node = schema.Bool()
-    storage_path = str(tmpdir.join('test_load.npz'))
-    storage = frontend.create(storage_path, schema_node)
+    storage = frontend.create(backend.storage_path, schema_node)
     storage.data.replace(True)
     storage.save()
-    frontend.load(storage_path)
+
+    frontend.load(backend.storage_path)
 
 
-def test_load_require_valid_fail(tmpdir):
+def test_load_require_valid_fail(backend):
     schema_node = schema.String(max_length=3)
-    storage_path = str(tmpdir.join('test_load.npz'))
-    storage = frontend.create(storage_path, schema_node)
+    storage = frontend.create(backend.storage_path, schema_node)
     storage.data.replace('spam')
     storage.save()
+
     with pytest.raises(schema.ValidationError):
-        frontend.load(storage_path)
+        frontend.load(backend.storage_path)
