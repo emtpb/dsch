3fc50ba6be602fdf1aae09b25df61042e2434afc
Manuel Webersen <webersen@emt.uni-paderborn.de>
2017-05-08 18:26:45 +0200

    Restructure tests for npz item nodes.
    Only leave tests for functionality that is not covered elsewhere.

--- tests/test_backend_npz.py
+++ tests/test_backend_npz.py
@@ -1,46 +1,26 @@
 import datetime
 import json
 import numpy as np
-from dsch import schema
+import pytest
+from dsch import data, schema
 from dsch.backends import npz
 
 
-class TestArray:
-    def test_init_from_storage(self):
-        data_node = npz.Array(schema.Array(dtype='int'), parent=None,
-                              data_storage=np.array([23, 42]))
-        assert np.all(data_node._storage == np.array([23, 42]))
-        assert np.all(data_node.value == np.array([23, 42]))
-
-    def test_replace(self):
-        data_node = npz.Array(schema.Array(dtype='int'), parent=None)
-        data_node.replace(np.array([23, 42]))
-        assert isinstance(data_node._storage, np.ndarray)
-        assert np.all(data_node._storage == np.array([23, 42]))
-
-    def test_save(self):
-        data_node = npz.Array(schema.Array(dtype='int'), parent=None)
-        data_node.replace(np.array([23, 42]))
-        assert np.all(data_node.save() == np.array([23, 42]))
-
-
-class TestBool:
-    def test_init_from_storage(self):
-        data_node = npz.Bool(schema.Bool(), parent=None,
-                             data_storage=np.array([True]))
-        assert data_node._storage == np.array([True])
-        assert data_node.value is True
-
-    def test_replace(self):
-        data_node = npz.Bool(schema.Bool(), parent=None)
-        data_node.replace(False)
-        assert isinstance(data_node._storage, np.ndarray)
-        assert data_node._storage == np.array([False])
-
-    def test_save(self):
-        data_node = npz.Bool(schema.Bool(), parent=None)
-        data_node.replace(False)
-        assert data_node.save() == np.array([False])
+@pytest.mark.parametrize('schema_node,valid_data', (
+    (schema.Array(dtype='int'), np.array([23, 42])),
+    (schema.Bool(), True),
+    (schema.Date(), datetime.date.today()),
+    (schema.DateTime(), datetime.datetime.now()),
+    (schema.Scalar(dtype='int32'), np.int32(42)),
+    (schema.String(), 'spam'),
+    (schema.Time(), datetime.datetime.now().time()),
+))
+def test_save_item_node(schema_node, valid_data):
+    data_node = data.data_node_from_schema(schema_node,
+                                           module_name='dsch.backends.npz',
+                                           parent=None)
+    data_node.replace(valid_data)
+    assert np.all(data_node.save() == data_node._storage)
 
 
 class TestCompilation:
@@ -65,78 +45,6 @@ class TestCompilation:
         assert 'eggs' in data_storage
         assert data_storage['spam'] == np.array([True])
         assert data_storage['eggs'] == np.array([False])
-
-
-class TestDate:
-    def test_init_from_storage(self):
-        dt = datetime.date.today()
-        data_storage = np.array([dt.year, dt.month, dt.day])
-        data_node = npz.Date(schema.Date(), parent=None,
-                             data_storage=data_storage)
-        assert np.all(data_node._storage == data_storage)
-        assert data_node.value == dt
-
-    def test_replace(self):
-        data_node = npz.Date(schema.Date(), parent=None)
-        dt = datetime.date.today()
-        data_node.replace(dt)
-        assert isinstance(data_node._storage, np.ndarray)
-        assert np.all(data_node._storage == np.array(
-            [dt.year, dt.month, dt.day]))
-
-    def test_save(self):
-        data_node = npz.Date(schema.Date(), parent=None)
-        dt = datetime.date.today()
-        data_node.replace(dt)
-        assert np.all(data_node.save() == np.array(
-            [dt.year, dt.month, dt.day]))
-
-
-class TestDateTime:
-    def test_init_from_storage(self):
-        dt = datetime.datetime.now()
-        data_storage = np.array([dt.year, dt.month, dt.day, dt.hour,
-                                 dt.minute, dt.second, dt.microsecond])
-        data_node = npz.DateTime(schema.DateTime(), parent=None,
-                                 data_storage=data_storage)
-        assert np.all(data_node._storage == data_storage)
-        assert data_node.value == dt
-
-    def test_replace(self):
-        data_node = npz.DateTime(schema.DateTime(), parent=None)
-        dt = datetime.datetime.now()
-        data_node.replace(dt)
-        assert isinstance(data_node._storage, np.ndarray)
-        assert np.all(data_node._storage == np.array(
-            [dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second,
-             dt.microsecond]))
-
-    def test_save(self):
-        data_node = npz.DateTime(schema.DateTime(), parent=None)
-        dt = datetime.datetime.now()
-        data_node.replace(dt)
-        assert np.all(data_node.save() == np.array(
-            [dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second,
-             dt.microsecond]))
-
-
-class TestScalar:
-    def test_init_from_storage(self):
-        data_node = npz.Scalar(schema.Scalar(dtype='int32'), parent=None,
-                               data_storage=np.int32(42))
-        assert data_node._storage == np.int32(42)
-        assert data_node.value == 42
-
-    def test_replace(self):
-        data_node = npz.Scalar(schema.Scalar(dtype='int32'), parent=None)
-        data_node.replace(42)
-        assert isinstance(data_node._storage, np.int32)
-        assert data_node._storage == 42
-
-    def test_save(self):
-        data_node = npz.Scalar(schema.Scalar(dtype='int32'), parent=None)
-        data_node.replace(42)
-        assert data_node.save() == np.int32(42)
 
 
 class TestStorage:
@@ -268,52 +176,6 @@ class TestList:
         assert len(data_storage) == 2
         assert data_storage['item_0'] == np.array([True])
         assert data_storage['item_1'] == np.array([False])
-
-
-class TestString:
-    def test_init_from_storage(self):
-        data_storage = np.array('spam', dtype='U')
-        data_node = npz.String(schema.String(), parent=None,
-                               data_storage=data_storage)
-        assert data_node._storage == data_storage
-        assert data_node.value == 'spam'
-
-    def test_replace(self):
-        data_node = npz.String(schema.String(), parent=None)
-        data_node.replace('spam')
-        assert isinstance(data_node._storage, np.ndarray)
-        assert data_node._storage == np.array('spam', dtype='U')
-
-    def test_save(self):
-        data_node = npz.String(schema.String(), parent=None)
-        data_node.replace('spam')
-        assert data_node.save() == np.array('spam', dtype='U')
-
-
-class TestTime:
-    def test_init_from_storage(self):
-        dt = datetime.time(13, 37, 42, 23)
-        data_storage = np.array([dt.hour, dt.minute, dt.second,
-                                 dt.microsecond])
-        data_node = npz.Time(schema.Time(), parent=None,
-                             data_storage=data_storage)
-        assert np.all(data_node._storage == data_storage)
-        assert data_node.value == dt
-
-    def test_replace(self):
-        data_node = npz.Time(schema.Time(), parent=None)
-        dt = datetime.time(13, 37, 42, 23)
-        data_node.replace(dt)
-        assert isinstance(data_node._storage, np.ndarray)
-        assert np.all(data_node._storage == np.array(
-            [dt.hour, dt.minute, dt.second, dt.microsecond]))
-
-    def test_save(self):
-        data_node = npz.Time(schema.Time(), parent=None)
-        dt = datetime.time(13, 37, 42, 23)
-        data_node.replace(dt)
-        assert np.all(data_node.save() == np.array(
-            [dt.hour, dt.minute, dt.second, dt.microsecond]))
 
 
 def test_inflate_dotted():
